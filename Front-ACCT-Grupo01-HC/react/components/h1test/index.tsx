3b2d2ce51544d20c91a6cb2b25c37c6ae8ec79d7
rafamoliv <rafaoliv.ti@gmail.com>
2021-08-28 18:57:32 -0300

    add styled-components

--- Front-ACCT-Grupo01-HC/react/components/h1test/index.tsx
+++ Front-ACCT-Grupo01-HC/react/components/h1test/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-//import "./style.css";
+import { Paragraph } from "./styled";
 
 const H1test = () => {
   return (
     <div>
-      <p>
+      <Paragraph>
         Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000
         years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from
         a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from
@@ -64,7 +64,7 @@ const H1test = () => {
         the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
         1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus
         Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-      </p>
+      </Paragraph>
     </div>
   );
 };
---
+++ Front-ACCT-Grupo01-HC/react/components/h1test/styled.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const Paragraph = styled.p`
+    color: red;
+`
